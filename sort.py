507fcf3d966400c6087a16c85f0516c14bcb1b6d
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-06-03 15:38:50 +0200

    [Update] Global update

--- sort.py
+++ sort.py
@@ -3,27 +3,35 @@ import os.path
 from os.path import isfile, join
 from os import listdir
 
-path = "data_sort"
+temp_path = "data_sort"
 final_data = ""
+sorted_list = "data_base_word_shorted.txt"
+db_list = "data_base_word.txt"
+count = 0
 
-if not os.path.isdir(path):
-    os.mkdir(path)
+if not os.path.isdir(temp_path):
+    os.mkdir(temp_path)
 
-with open("data_base_word.txt") as file:
+# Count number of lines
+with open(db_list, 'r') as fp:
+    for count, line in enumerate(fp):
+        pass
+
+# Sorting
+with open(db_list) as file:
     for idx,line in enumerate(file):
-        print(str(idx) + " => " + str(len(line.rstrip())))
-        f = open(path+"/"+str("{:02d}".format(len(line.rstrip())))+".txt", "a")
+        print("["+str(idx+1)+"/"+str(count)+"] " + line.rstrip() + " => " + str(len(line.rstrip())))
+        f = open(temp_path+"/"+str("{:02d}".format(len(line.rstrip())))+".txt", "a")
         f.write(line)
         f.close()
 
-ls_file = [f for f in listdir(path) if isfile(join(path, f))]
+# Merge data
+ls_file = [f for f in listdir(temp_path) if isfile(join(temp_path, f))]
 
-print(ls_file)
-
+# Wirte data
 for file in ls_file:
-    with open(path+"/"+file) as fp:
+    with open(temp_path+"/"+file) as fp:
         final_data += fp.read()
-
-f = open("data_base_word_shorted.txt", "a")
+f = open(sorted_list, "a")
 f.write(final_data)
 f.close()
